dc348d7764cb94dab5c85b6484299098a7a4aeea
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2016-09-02 13:35:50 +0100

    Run PAA on sectors and countries.

--- r/protective-asset-allocation.r
+++ r/protective-asset-allocation.r
@@ -22,6 +22,7 @@ getPrices <- function(symbols, frequency) {
     colnames(adjustedPrices) <- gsub("\\.[A-z]*", "", colnames(adjustedPrices))
     prices[[i]] <-
       Cl(switch(frequency,
+                daily = to.daily(adjustedPrices),
                 weekly = to.weekly(adjustedPrices),
                 monthly = to.monthly(adjustedPrices),
                 quarterly = to.quarterly(adjustedPrices),
@@ -74,33 +75,10 @@ PAA <- function(risk_on, risk_off, frequency, lookback, protection, top) {
   
   strategy <- lag(weights) * returns
   strategy.returns <- xts(rowSums(strategy), order.by = index(returns))
+  colnames(strategy.returns) <- paste0(frequency, ".returns")
   return(strategy.returns)
 }
 
-risk_on <- c("SPY", # SPDR S&P 500 ETF Trust
-             "QQQ", # NASDAQ- 100 Index Tracking Stock
-             "EFA", # iShares MSCI EAFE Index Fund (ETF)
-             "EEM", # iShares MSCI Emerging Markets Indx (ETF)
-             "GLD", # SPDR Gold Trust (ETF)
-             "GSG", # iShares S&P GSCI Commodity-Indexed Trust
-             "IYR", # iShares Dow Jones US Real Estate (ETF)
-             "UUP", # PowerShares DB US Dollar Bullish ETF
-             "HYG", # iShares iBoxx $ High Yid Corp Bond (ETF)
-             "LQD", # iShares IBoxx $ Invest Grade Corp Bd Fd
-             "IWM") # iShares Russell 2000 Index (ETF)
-risk_off <- c("SHY", # iShares 1-3 Year Treasury Bond
-              "IEF", # iShares Barclays 7-10 Year Trasry Bnd Fd
-              "AGG", # iShares Barclays Aggregate Bond Fund
-              "TLT") # iShares Barclays 20+ Yr Treas.Bond (ETF)
-
-symbols <- c(risk_on, risk_off)
-getSymbols(symbols, from="1990-01-01")
-strategy.returns <- PAA(risk_on, risk_off, frequency = "weekly", lookback = 52, protection = 2, top = 4)
-printPerformance(strategy.returns)
-
-#filename <- "paa.pdf"
-#pdf(filename)
-#sink(filename, append=TRUE, split=TRUE)
 
 printPerformance <- function(strategy.returns) {
   layout(rbind(c(1,2),c(3,4)))
@@ -110,14 +88,101 @@ printPerformance <- function(strategy.returns) {
   chart.TimeSeries(strategy.returns)
   chart.Histogram(strategy.returns, main = "Density", breaks=40, methods = c("add.density", "add.normal", "add.centered", "add.rug"))
   #charts.RollingPerformance(strategy.returns, width = 52)
-  rbind(
-    table.Stats(strategy.returns),
-    table.AnnualizedReturns(strategy.returns),
-    table.Drawdowns(strategy.returns, top = 10),
-    table.DrawdownsRatio(strategy.returns)
-  )
-  #table.CalendarReturns(strategy.returns)
+  
+  table.Stats(strategy.returns)
+  table.AnnualizedReturns(strategy.returns)
+  table.Drawdowns(strategy.returns, top = 10)
+  table.DrawdownsRatio(strategy.returns)
+  table.CalendarReturns(strategy.returns)
 }
+
+
+risk_on <- c("SPY", # SPDR S&P 500 ETF Trust
+             # "QQQ", # NASDAQ- 100 Index Tracking Stock
+              "EFA")#, # iShares MSCI EAFE Index Fund (ETF)
+             # "EEM", # iShares MSCI Emerging Markets Indx (ETF)
+             # "GLD", # SPDR Gold Trust (ETF)
+             # "GSG", # iShares S&P GSCI Commodity-Indexed Trust
+             # "IYR", # iShares Dow Jones US Real Estate (ETF)
+             # "UUP", # PowerShares DB US Dollar Bullish ETF
+             # "HYG", # iShares iBoxx $ High Yid Corp Bond (ETF)
+             # "LQD", # iShares IBoxx $ Invest Grade Corp Bd Fd
+             # "IWM") # iShares Russell 2000 Index (ETF)
+
+
+# risk_on <- c("XBI", "XLB", "XLE", "XLF", "XLI", "XLK", "XLP", "XLU", "XLV", "XLY")
+symbols.country <- c(
+  "UBR", # ProShares Ultra MSCI Brazil	
+  "BRF", # Market Vectors Brazil Small-Cap ETF	
+  "EPU", # iShares MSCI All Peru Capped Idx		
+  "EWZ", # iShares MSCI Brazil Index Fund		
+  "IDX", # Market Vectors Indonesia ETF		
+  "EIDO", # iShares MSCI Indonesia Investable Market		
+  "PGJ", # PowerShares Golden Dragon Halter USX China Portfolio		
+  "EWY", # iShares MSCI South Korea Index Fund		
+  "RBL", # SPDR S&P Russia ETF	
+  "THD", # iShares MSCI Thailand Invest Mkt Index		
+  "RSX", # Market Vectors Russia ETF Trust		
+  "GXC", # SPDR S&P China ETF		
+  "EWT", # iShares MSCI Taiwan Index Fund		
+  "EWH", # iShares MSCI Hong Kong Index Fund		
+  "EWA", # iShares MSCI Australia Index Fund	
+  "XPP", # ProShares Ultra FTSE/Xinhua China 25		
+  "EPI", # WisdomTree India Earnings		
+  "GXG", # Global X/InterBolsa FTSE Colombia 20 ETF		
+  "SPY", # SPDR S&P 500		
+  "QQQ", # PowerShares QQQ	
+  "EWM", # iShares MSCI Malaysia Index Fund		
+  "FXI", # IShares China Large Cap ETF		
+  "EWC", # iShares MSCI Canada Index Fund		
+  "EWK", # iShares MSCI Belgium Index Fund		
+  "IWM", # iShares Russell 2000 Index Fund		
+  "INP", # iPath MSCI India ETN		
+  "INDL", # Direxion Daily India Bull 2x Shares	
+  "PIN", # PowerShares India		
+  "ECH", # iShares MSCI Chile Index Fund		
+  "EZJ", # ProShares Ultra MSCI Japan		
+  "EWJ", # iShares MSCI Japan Index Fund		
+  "EDEN", # iShares MSCI Denmark Cppd Investable Mkt	
+  "EWO"	, # iShares MSCI Austria Index Fund	
+  "EWN", # iShares MSCI Netherlands Index Fund		
+  "EWS", # iShares MSCI Singapore Index Fund		
+  "TUR", # iShares MSCI Turkey Invest Mkt Index		
+  "EWG", # iShares MSCI Germany Index Fund		
+  "DAX", # Recon Capital DAX Germany ETF		
+  "EGPT", # Market Vectors Egypt Index ETF	
+  "EWW", # iShares MSCI Mexico Index Fund	
+  "EWQ", # iShares MSCI France Index Fund	
+  "EIRL", # iShares MSCI Ireland Capped Investable Market	
+  "EWD", # iShares MSCI Sweden Index Fund	
+  "EZA", # iShares MSCI South Africa Index Fund	
+  "EWL"	, # iShares MSCI Switzerland Index Fund	
+  "EWU", # iShares MSCI United Kingdom Index Fund	
+  "EIS", # iShares MSCI Israel Cap Invest Mkt Index	
+  "UMX", # ProShares Ultra MSCI Mexico Investable Market	
+  "EWP", # iShares MSCI Spain Index Fund	
+  "EPOL", # iShares MSCI Poland Investable Market Index Fund	
+  "YXI", # ProShares Short FTSE/Xinhua China 25	
+  "EWI", # iShares MSCI Italy Index Fund	
+  "EWV", # ProShares UltraShort MSCI Japan		
+  "SMK", # ProShares UltraShort MSCI Mexico		
+  "FXP", # ProShares UltraShort FTSE/Zinhua China 25	
+  "BZQ" # ProShares UltraShort MSCI Brazil		
+)
+
+risk_off <- c("SHY", # iShares 1-3 Year Treasury Bond
+              "IEF", # iShares Barclays 7-10 Year Trasry Bnd Fd
+              "AGG", # iShares Barclays Aggregate Bond Fund
+              "TLT") # iShares Barclays 20+ Yr Treas.Bond (ETF)
+
+symbols <- c(symbols.country, risk_off)
+getSymbols(symbols, from="1990-01-01")
+strategy.returns <- PAA(symbols.country, risk_off, frequency = "weekly", lookback = 13, protection = 2, top = 9)
+printPerformance(strategy.returns)
+
+#filename <- "paa.pdf"
+#pdf(filename)
+#sink(filename, append=TRUE, split=TRUE)
 
 
 #sink()
