a16f4aae190c61d0506b0569ea3f11e8ba97bf77
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2016-09-08 14:40:32 +0100

    Check in the initial file for volatility adjusted momentum.

--- r/error-adjusted-momentum.r
+++ r/error-adjusted-momentum.r
@@ -2,6 +2,7 @@
 library(TTR)
 library(zoo)
 library(quantmod)
+library(PerformanceAnalytics)
 
 # download daily S&P 500 prices from Dec 31, 1990 forward
 gspc <-getSymbols('^gspc', from='1990-12-31', auto.assign=FALSE)
@@ -73,3 +74,52 @@ r <- sapply(10:365, FUN = function(x) {
   #rownames(result) <- c("Annualized Return", "Annualized Std Dev", "Annualized Sharpe (Rf=0%)")
   return(result)
 })
+
+
+
+
+
+
+
+
+eam <-function(symbol, lookback, sma) { # x=ticker, y=lookback period for forecast z=SMA period
+  return <- na.omit(Return.calculate(Ad(symbol)))
+  returns.trailing <-na.omit(SMA(return, lookback)) # forecast based on "y" trailing period returns
+  returns.trailing.lagged <-na.omit(Lag(returns.trailing, k=1)) # lag forecasts by 1 period
+  d <- na.omit(cbind(returns.trailing.lagged, return)) # combine lagged forecasts with actual returns into one file
+  e <- as.xts(apply(d, 1, diff)) # actual daily return less forecast
+  f <- to.daily(na.omit(rollapplyr(e, lookback, function(x) mean(abs(x)))),drop.time=TRUE,OHLC=FALSE) # mean absolute error
+  g <- cbind(return,f) # combine actual return with MAE into one file
+  h <- na.omit(return[,1]/g[,2]) # divide actual return by MAE
+  i <- na.omit(SMA(h,sma)) # generate 200-day moving average of adjusted return
+  j <- na.omit(Lag(ifelse(i >0,1,0))) # lag adjusted return signal by one day for trading analysis
+}
+
+# function to generate raw EAM signal data
+eam.ret <-function(x,y,z) { # x=ticker, y=lookback period for vol forecast, z=SMA period
+  a <-eam(x,y,z)
+  b <-na.omit(ROC(Ad(x),1,"discrete"))
+  
+  c <-length(a)-1
+  d <-tail(b,c)
+  e <-d*a
+  f <-cumprod(c(100,1 + e))
+  
+  g <-tail(b,c)
+  h <-cumprod(c(100,1 + g))
+  
+  i <-cbind(f,h)
+  colnames(i) <-c("model","asset")
+  
+  date.a <-c((first(tail((as.Date(index(x))),c))-1),(tail((as.Date(index(x))),c)))
+  
+  j <-xts(i,date.a)
+  
+  return(j)
+  
+}
+
+
+
+
+
