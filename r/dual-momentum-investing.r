24dcc8c059596c0df46bfb6cc4730759d611178e
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2016-10-08 11:38:29 +0100

    Parametrised the entire absolute momentum analysis

--- r/dual-momentum-investing.r
+++ r/dual-momentum-investing.r
@@ -108,6 +108,37 @@ getSymbols(c(gem.symbols, cash), from="1990-01-01")
 prices <- getPrices(gem.symbols, frequency)
 returns <- getReturns(gem.symbols, frequency)
 
+
+getSymbols("^IRX", from="1900-01-01")
+getSymbols("^GSPC", from="1900-01-01")
+
+irx.dailyYield <- (1+(Cl(IRX)/100))^(1/252) - 1
+irx.dailyPrice <- cumprod(1+dailyYield)
+
+gspc.weekly <- to.weekly(GSPC)
+irx.weekly <- to.weekly(irx.dailyPrice)
+
+gspc.returns <- Return.calculate(Ad(gspc.weekly))
+irx.returns <- Return.calculate(Cl(irx.weekly))                                 
+
+startDate <- max(min(index(gspc.returns)), min(index(irx.returns)))
+startDateFilter <- paste0(startDate,"::")
+gspc.tr52wk8wk <- totalReturn(Ad(gspc.weekly), 52, 8)[startDateFilter]
+irx.tr52wk <- totalReturn(Cl(irx.weekly), 52, 0)[startDateFilter]
+
+signal <- ifelse(gspc.tr52wk8wk > irx.tr52wk, 1, 0)
+
+strategy <- lag(signal) * gspc.returns[startDateFilter] + lag(1-signal) * irx.returns[startDateFilter]
+
+strategies <- cbind(gspc.returns, irx.returns, strategy) 
+colnames(strategies) <- c("GSPC", "TBill", "GSPC Absolute momentum")
+
+
+
+
+
+
+
 getSymbols( "SPY", from="1900-01-01" )
 getSymbols( "SHY", from="1900-01-01" )
 
@@ -121,6 +152,35 @@ totalReturn <- function(prices, longPeriod, shortPeriod) {
   ROC(prices, n = longPeriod, type = "discrete") - ROC(prices, n = shortPeriod, type = "discrete")
 }
 
+absoluteMomentum <- function(symbol, cash, longMomenrum = 52, shortMomentum = 0) {
+  frequency <- "weekly"
+  symbol.weekly <- getPrices(symbol, frequency)
+  cash.weekly <- getPrices(cash, frequency)
+  
+  symbol.returns <- getReturns(symbol, frequency)
+  cash.returns <- getReturns(cash, frequency)
+  
+  startDate <- max(min(index(symbol.returns)), min(index(cash.returns)))
+  startDateFilter <- paste0(startDate,"::")
+  
+  symbol.tr52wk8wk <- totalReturn(Ad(symbol.weekly), longMomenrum, shortMomentum)[startDateFilter]
+  cash.tr52wk <- totalReturn(Ad(cash.weekly), longMomenrum, 0)[startDateFilter]
+  
+  signal <- ifelse(symbol.tr52wk8wk > cash.tr52wk, 1, 0)
+  
+  strategy <- 
+      lag(signal) * symbol.returns[startDateFilter] 
+    + lag(1-signal) * cash.returns[startDateFilter]
+  
+  
+  strategies <- cbind(symbol.returns, cash.returns, strategy)
+  colnames(strategies) <- c(symbol, cash, paste0(symbol," - Absolute momentum"))
+  
+  return(strategies)
+}
+
+absoluteMomentum("SPY", "SHY")
+
 spy.tr52wk8wk <- totalReturn(Ad(spy.weekly), 52, 8)["2003::"]
 shy.tr52wk <- totalReturn(Ad(shy.weekly), 52, 0)["2003::"]
 
@@ -128,11 +188,14 @@ signal <- ifelse(spy.tr52wk8wk > shy.tr52wk, 1, 0)
 
 strategy <- lag(signal) * spy.returns["2003::"] + lag(1-signal) * shy.returns["2003::"]
 
+strategies <- cbind(spy.returns, shy.returns, strategy)
+colnames(strategies) <- c("S&P", "T-Bill", "S&P Absolute momentum")
+
 dev.new()
-charts.PerformanceSummary(strategy)
-t(table.AnnualizedReturns(strategy))
-table.DownsideRisk(strategy)
-table.DrawdownsRatio(strategy)
+charts.PerformanceSummary(strategies)
+t(table.AnnualizedReturns(strategies))
+table.DownsideRisk(strategies)
+table.DrawdownsRatio(strategies)
 
 
 dev.new()
--- r/volatility-adjusted-momentum.r
+++ r/volatility-adjusted-momentum.r
@@ -2,12 +2,110 @@ require(xts)
 require(quantmod)
 require(PerformanceAnalytics)
 require(TTR)
+library(Quandl)
 
+setwd("D:/Github/quant-intro/r")
+setwd("C:/Code/github/quant-intro/r")
 
-# download daily S&P 500 prices from Dec 31, 1990 forward
+source("secret.r")
+Quandl.api_key(api_key = quandl.apikey)
+
+# download daily S&P 500 prices from 1 Jan 1950 forward
 gspc <-getSymbols('^gspc', from='1900-12-31', auto.assign=FALSE)
 
-gspc.returns <- Return.calculate(Cl(gspc))
+volAdjustedSmaCrossOver <- function(periods, pcs, calc = "close", vol.periods = 10) {
+  rtns <- Return.calculate(Cl(pcs))
+  vol <- volatility(pcs, calc = calc, n = vol.periods)
+  rtns.voladj <- rtns / vol
+  sma <- SMA(rtns.voladj, periods)
+  signal <- ifelse(sma >0,1,0)
+  returns <- lag(signal) * rtns
+  returns <- na.omit(returns)
+  return(returns)
+}
+
+strategy.volsd <- volAdjustedSmaCrossOver(110, gspc, calc = "close", vol.periods = 200)
+strategy.volyz <- volAdjustedSmaCrossOver(110, gspc, calc = "yang.zhang", vol.periods = 200)
+strategy.buyandhold <- Return.calculate(Cl(gspc))
+colnames(strategy.volsd) <- "volsd"
+colnames(strategy.volyz) <- "volyz"
+colnames(strategy.buyandhold) <- "Buy & hold"
+
+getStrategies <- function(date) {
+  return(cbind(strategy.volyz[date], 
+               strategy.volsd[date], 
+               strategy.buyandhold[date]))
+}
+
+strategies <- getStrategies("2009-03-01::")
+rbind(
+  table.AnnualizedReturns(strategies),
+  maxDrawdown(strategies),
+  CalmarRatio(strategies))
+
+sr <- matrix(nrow = 365, ncol = 365)
+for(sma.window in seq(10, 365, 10)) {
+  for(vol.window in seq(10, 365, 10)) {
+    strategy <- volAdjustedSmaCrossOver(sma.window, gspc, calc = "close", vol.periods = vol.window)
+    colnames(strategy) <- paste0(sma.window, " ", vol.window)
+    r <- table.AnnualizedReturns(strategy)
+    sr[sma.window, vol.window] <- r[3,]
+  }
+}
+
+
+ief <- getSymbols("ief", from="1900-12-31", auto.assign=FALSE)
+ief.returns <- Return.calculate(Ad(ief)) 
+ief.volsd <- volAdjustedSmaCrossOver(110, ief, calc = "close", vol.periods = 200)
+
+efa <- getSymbols("efa", from="1900-12-31", auto.assign=FALSE)
+efa.returns <- Return.calculate(Ad(efa)) 
+efa.volsd <- volAdjustedSmaCrossOver(110, efa, calc = "close", vol.periods = 200)
+
+eem <- getSymbols("eem", from="1900-12-31", auto.assign=FALSE)
+eem.returns <- Return.calculate(Ad(eem)) 
+eem.volsd <- volAdjustedSmaCrossOver(110, eem, calc = "close", vol.periods = 200)
+
+gld <- getSymbols("gld", from="1900-12-31", auto.assign=FALSE)
+gld.returns <- Return.calculate(Ad(gld)) 
+gld.volsd <- volAdjustedSmaCrossOver(110, gld, calc = "close", vol.periods = 200)
+
+iyr <- getSymbols("iyr", from="1900-12-31", auto.assign=FALSE)
+iyr.returns <- Return.calculate(Ad(iyr)) 
+iyr.volsd <- volAdjustedSmaCrossOver(110, iyr, calc = "close", vol.periods = 200)
+
+
+uup <- getSymbols("uup", from="1900-12-31", auto.assign=FALSE)
+uup.returns <- Return.calculate(Ad(uup)) 
+uup.volsd <- volAdjustedSmaCrossOver(110, uup, calc = "close", vol.periods = 200)
+
+
+volAdjustedSemiVarSmaCrossOver <- function(periods, pcs, calc = "close", vol.periods = 10) {
+  rtns <- Return.calculate(Cl(pcs))
+  rtns <- rtns[rtns<0]
+  vol <- volatility(pcs, calc = calc, n = vol.periods)
+  rtns.voladj <- rtns / vol
+  sma <- SMA(rtns.voladj, periods)
+  signal <- ifelse(sma >0,1,0)
+  returns <- lag(signal) * rtns
+  returns <- na.omit(returns)
+  return(returns)
+}
+
+
+
+"QQQ", # NASDAQ- 100 Index Tracking Stock
+"EFA", #, # iShares MSCI EAFE Index Fund (ETF)
+"EEM", # iShares MSCI Emerging Markets Indx (ETF)
+"GLD", # SPDR Gold Trust (ETF)
+"GSG", # iShares S&P GSCI Commodity-Indexed Trust
+"IYR", # iShares Dow Jones US Real Estate (ETF)
+"UUP", # PowerShares DB US Dollar Bullish ETF
+"HYG", # iShares iBoxx $ High Yid Corp Bond (ETF)
+"LQD", # iShares IBoxx $ Invest Grade Corp Bd Fd
+"IWM") # iShares Russell 2000 Index (ETF)
+
+
 
 
 gspc.volatility.yang.zhang <- volatility(gspc, calc = "yang.zhang", n = 10)
