fcf31301716a1c35f50e8c7fb9b750d6c8580bf2
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2016-10-09 09:24:40 +0100

    Find optimal long term/short term momentum lookbacks for various asset classes

--- r/dual-momentum-investing.r
+++ r/dual-momentum-investing.r
@@ -136,18 +136,6 @@ colnames(strategies) <- c("GSPC", "TBill", "GSPC Absolute momentum")
 
 
 
-
-
-
-getSymbols( "SPY", from="1900-01-01" )
-getSymbols( "SHY", from="1900-01-01" )
-
-spy.weekly <- to.weekly(SPY)
-shy.weekly <- to.weekly(SHY)
-
-spy.returns <- Return.calculate(Ad(spy.weekly))
-shy.returns <- Return.calculate(Ad(shy.weekly))
-
 totalReturn <- function(prices, longPeriod, shortPeriod) {
   ROC(prices, n = longPeriod, type = "discrete") - ROC(prices, n = shortPeriod, type = "discrete")
 }
@@ -163,8 +151,8 @@ absoluteMomentum <- function(symbol, cash, longMomenrum = 52, shortMomentum = 0)
   startDate <- max(min(index(symbol.returns)), min(index(cash.returns)))
   startDateFilter <- paste0(startDate,"::")
   
-  symbol.tr52wk8wk <- totalReturn(Ad(symbol.weekly), longMomenrum, shortMomentum)[startDateFilter]
-  cash.tr52wk <- totalReturn(Ad(cash.weekly), longMomenrum, 0)[startDateFilter]
+  symbol.tr52wk8wk <- totalReturn(symbol.weekly, longMomenrum, shortMomentum)[startDateFilter]
+  cash.tr52wk <- totalReturn(cash.weekly, longMomenrum, 0)[startDateFilter]
   
   signal <- ifelse(symbol.tr52wk8wk > cash.tr52wk, 1, 0)
   
@@ -179,23 +167,111 @@ absoluteMomentum <- function(symbol, cash, longMomenrum = 52, shortMomentum = 0)
   return(strategies)
 }
 
-absoluteMomentum("SPY", "SHY")
 
-spy.tr52wk8wk <- totalReturn(Ad(spy.weekly), 52, 8)["2003::"]
-shy.tr52wk <- totalReturn(Ad(shy.weekly), 52, 0)["2003::"]
 
-signal <- ifelse(spy.tr52wk8wk > shy.tr52wk, 1, 0)
+findOptimalPeriods <- function(asset.symbol) {
 
-strategy <- lag(signal) * spy.returns["2003::"] + lag(1-signal) * shy.returns["2003::"]
+  ## OPTIMISATION
+  sharpe <- matrix(ncol = 14, nrow = 40)
+  rtn <- matrix(ncol = 14, nrow = 40)
 
-strategies <- cbind(spy.returns, shy.returns, strategy)
-colnames(strategies) <- c("S&P", "T-Bill", "S&P Absolute momentum")
+  for (long in 13:52) {
+    for (short in 0:13) {
+      str <- absoluteMomentum(asset.symbol, "SHY", long, short)
+      t <- table.AnnualizedReturns(str)
+      sharpe[long - 12, short + 1] <- t[3,3]
+      rtn[long - 12, short + 1] <- t[1,3]
+    }
+  }
+  offset <- c(12, -1)
+  maxSharpe <- which(sharpe == max(sharpe, na.rm = TRUE), arr.ind = TRUE)
+  
+  write.csv(sharpe, file = paste0(asset.symbol, "-sharpe.csv"))
+  write.csv(rtn, file = paste0(asset.symbol, "-returns.csv"))
+  
+  maxSharpePeriod <- maxSharpe + offset
+  return(maxSharpePeriod)
+}
+gem.symbols <- c("IVV", "BND", "VEU")
+symbols.sectors <- c("XBI", "XLB", "XLE", "XLF", "XLI", "XLK", "XLP", "XLU", "XLV", "XLY")
+getSymbols(c("SPY", "SHY", "IVV", "BND", "VEU"), from="1900-01-01")
+getSymbols(symbols.sectors, from="1900-01-01")
+symbols.country <- c(
+  "UBR", # ProShares Ultra MSCI Brazil	
+  "EWZ", # iShares MSCI Brazil Index Fund		
+  "IDX", # Market Vectors Indonesia ETF		
+  "EIDO", # iShares MSCI Indonesia Investable Market		
+  "PGJ", # PowerShares Golden Dragon Halter USX China Portfolio		
+  "EWY", # iShares MSCI South Korea Index Fund		
+  "RBL", # SPDR S&P Russia ETF	
+  "THD", # iShares MSCI Thailand Invest Mkt Index		
+  "RSX", # Market Vectors Russia ETF Trust		
+  "GXC", # SPDR S&P China ETF		
+  "EWT", # iShares MSCI Taiwan Index Fund		
+  "EWH", # iShares MSCI Hong Kong Index Fund		
+  "EWA", # iShares MSCI Australia Index Fund	
+  "EPI", # WisdomTree India Earnings		
+  "GXG", # Global X/InterBolsa FTSE Colombia 20 ETF		
+  "SPY", # SPDR S&P 500		
+  "QQQ", # PowerShares QQQ	
+  "EWM", # iShares MSCI Malaysia Index Fund		
+  "FXI", # IShares China Large Cap ETF		
+  "EWC", # iShares MSCI Canada Index Fund		
+  "EWK", # iShares MSCI Belgium Index Fund		
+  "IWM", # iShares Russell 2000 Index Fund		
+  "INP", # iPath MSCI India ETN		
+  "PIN", # PowerShares India		
+  "EWJ", # iShares MSCI Japan Index Fund		
+  "EWO"	, # iShares MSCI Austria Index Fund	
+  "EWN", # iShares MSCI Netherlands Index Fund		
+  "EWS", # iShares MSCI Singapore Index Fund		
+  "TUR", # iShares MSCI Turkey Invest Mkt Index		
+  "EWG", # iShares MSCI Germany Index Fund		
+  "EWW", # iShares MSCI Mexico Index Fund	
+  "EWQ", # iShares MSCI France Index Fund	
+  "EWD", # iShares MSCI Sweden Index Fund	
+  "EZA", # iShares MSCI South Africa Index Fund	
+  "EWL"	, # iShares MSCI Switzerland Index Fund	
+  "EWU", # iShares MSCI United Kingdom Index Fund	
+  "EIS", # iShares MSCI Israel Cap Invest Mkt Index	
+  "EWP", # iShares MSCI Spain Index Fund	
+  "EWI" # iShares MSCI Italy Index Fund	
+ )
+getSymbols(symbols.country, from="1900-01-01")
+
+
+symbols.paa <- c("SPY", # SPDR S&P 500 ETF Trust
+             "QQQ", # NASDAQ- 100 Index Tracking Stock
+             "EFA", #, # iShares MSCI EAFE Index Fund (ETF)
+             "EEM", # iShares MSCI Emerging Markets Indx (ETF)
+             "GLD", # SPDR Gold Trust (ETF)
+             "GSG", # iShares S&P GSCI Commodity-Indexed Trust
+             "IYR", # iShares Dow Jones US Real Estate (ETF)
+             "UUP", # PowerShares DB US Dollar Bullish ETF
+             "HYG", # iShares iBoxx $ High Yid Corp Bond (ETF)
+             "LQD", # iShares IBoxx $ Invest Grade Corp Bd Fd
+             "IWM") # iShares Russell 2000 Index (ETF)
+
+getSymbols(symbols.paa, from="1900-01-01")
+
+analysis.symbols <- c(symbols.sectors, symbols.country) 
+analysis.symbols <- symbols.paa
+for(sec in analysis.symbols) {
+  varname <- paste0(sec, ".maxSharpePeriod")
+  optimalPeriods <- findOptimalPeriods(sec)
+  assign(varname, optimalPeriods, envir = globalenv())
+  write.csv(optimalPeriods, file = paste0(varname, ".csv"))
+}
+
+
+str <- absoluteMomentum("XLV", "SHY", 37, 11)
+
 
 dev.new()
-charts.PerformanceSummary(strategies)
-t(table.AnnualizedReturns(strategies))
-table.DownsideRisk(strategies)
-table.DrawdownsRatio(strategies)
+charts.PerformanceSummary(str)
+t(table.AnnualizedReturns(str))
+table.DownsideRisk(str)
+table.DrawdownsRatio(str)
 
 
 dev.new()
@@ -203,3 +279,142 @@ layout(rbind(c(1),c(2), c(3)))
 chart.CumReturns(strategy)
 chart.TimeSeries(signal)
 chart.Drawdown(strategy)
+
+
+allsymbols.maxSharpePeriod <-
+  
+  rbind(
+    SPY.maxSharpePeriod, # SPDR S&P 500 ETF Trust
+    QQQ.maxSharpePeriod, # NASDAQ- 100 Index Tracking Stock
+    EFA.maxSharpePeriod, #, # iShares MSCI EAFE Index Fund (ETF)
+    EEM.maxSharpePeriod, # iShares MSCI Emerging Markets Indx (ETF)
+    GLD.maxSharpePeriod, # SPDR Gold Trust (ETF)
+    GSG.maxSharpePeriod, # iShares S&P GSCI Commodity-Indexed Trust
+    IYR.maxSharpePeriod, # iShares Dow Jones US Real Estate (ETF)
+    UUP.maxSharpePeriod, # PowerShares DB US Dollar Bullish ETF
+    HYG.maxSharpePeriod, # iShares iBoxx $ High Yid Corp Bond (ETF)
+    LQD.maxSharpePeriod, # iShares IBoxx $ Invest Grade Corp Bd Fd
+    IWM.maxSharpePeriod, # iShares Russell 2000 Index (ETF)
+    
+    #sectors  
+    XBI.maxSharpePeriod, 
+    XLB.maxSharpePeriod, 
+    XLE.maxSharpePeriod, 
+    XLF.maxSharpePeriod, 
+    XLI.maxSharpePeriod, 
+    XLK.maxSharpePeriod, 
+    XLP.maxSharpePeriod, 
+    XLU.maxSharpePeriod, 
+    XLV.maxSharpePeriod, 
+    XLY.maxSharpePeriod,
+    
+    # countries
+    UBR.maxSharpePeriod,
+    EWZ.maxSharpePeriod,
+    IDX.maxSharpePeriod,
+    EIDO.maxSharpePeriod,
+    PGJ.maxSharpePeriod,
+    EWY.maxSharpePeriod,
+    RBL.maxSharpePeriod,
+    THD.maxSharpePeriod,
+    RSX.maxSharpePeriod,
+    GXC.maxSharpePeriod,
+    EWT.maxSharpePeriod,
+    EWH.maxSharpePeriod,
+    EWA.maxSharpePeriod, # iShares MSCI Australia Index Fund	
+  EPI.maxSharpePeriod, # WisdomTree India Earnings		
+  GXG.maxSharpePeriod, # Global X/InterBolsa FTSE Colombia 20 ETF		
+  SPY.maxSharpePeriod, # SPDR S&P 500		
+  QQQ.maxSharpePeriod, # PowerShares QQQ	
+  EWM.maxSharpePeriod, # iShares MSCI Malaysia Index Fund		
+  FXI.maxSharpePeriod, # IShares China Large Cap ETF		
+  EWC.maxSharpePeriod, # iShares MSCI Canada Index Fund		
+  EWK.maxSharpePeriod, # iShares MSCI Belgium Index Fund		
+  IWM.maxSharpePeriod, # iShares Russell 2000 Index Fund		
+  INP.maxSharpePeriod, # iPath MSCI India ETN		
+  PIN.maxSharpePeriod, # PowerShares India		
+  EWJ.maxSharpePeriod, # iShares MSCI Japan Index Fund		
+  EWO.maxSharpePeriod	, # iShares MSCI Austria Index Fund	
+  EWN.maxSharpePeriod, # iShares MSCI Netherlands Index Fund		
+  EWS.maxSharpePeriod, # iShares MSCI Singapore Index Fund		
+  TUR.maxSharpePeriod, # iShares MSCI Turkey Invest Mkt Index		
+  EWG.maxSharpePeriod, # iShares MSCI Germany Index Fund		
+  EWW.maxSharpePeriod, # iShares MSCI Mexico Index Fund	
+  EWQ.maxSharpePeriod, # iShares MSCI France Index Fund	
+  EWD.maxSharpePeriod, # iShares MSCI Sweden Index Fund	
+  EZA.maxSharpePeriod, # iShares MSCI South Africa Index Fund	
+  EWL.maxSharpePeriod, # iShares MSCI Switzerland Index Fund	
+  EWU.maxSharpePeriod, # iShares MSCI United Kingdom Index Fund	
+  EIS.maxSharpePeriod, # iShares MSCI Israel Cap Invest Mkt Index	
+  EWP.maxSharpePeriod, # iShares MSCI Spain Index Fund	
+  EWI.maxSharpePeriod # iShares MSCI Italy Index Fund	
+)
+
+rownames(allsymbols.maxSharpePeriod) <-
+  #sectors
+  c(
+    "SPY.maxSharpePeriod", # SPDR S&P 500 ETF Trust
+    "QQQ.maxSharpePeriod", # NASDAQ- 100 Index Tracking Stock
+    "EFA.maxSharpePeriod", #, # iShares MSCI EAFE Index Fund (ETF)
+    "EEM.maxSharpePeriod", # iShares MSCI Emerging Markets Indx (ETF)
+    "GLD.maxSharpePeriod", # SPDR Gold Trust (ETF)
+    "GSG.maxSharpePeriod", # iShares S&P GSCI Commodity-Indexed Trust
+    "IYR.maxSharpePeriod", # iShares Dow Jones US Real Estate (ETF)
+    "UUP.maxSharpePeriod", # PowerShares DB US Dollar Bullish ETF
+    "HYG.maxSharpePeriod", # iShares iBoxx $ High Yid Corp Bond (ETF)
+    "LQD.maxSharpePeriod", # iShares IBoxx $ Invest Grade Corp Bd Fd
+    "IWM.maxSharpePeriod", # iShares Russell 2000 Index (ETF)
+    
+    "XBI.maxSharpePeriod", 
+    "XLB.maxSharpePeriod", 
+    "XLE.maxSharpePeriod", 
+    "XLF.maxSharpePeriod", 
+    "XLI.maxSharpePeriod", 
+    "XLK.maxSharpePeriod", 
+    "XLP.maxSharpePeriod", 
+    "XLU.maxSharpePeriod", 
+    "XLV.maxSharpePeriod", 
+    "XLY.maxSharpePeriod",
+    
+    "UBR.maxSharpePeriod",
+    "EWZ.maxSharpePeriod",
+    "IDX.maxSharpePeriod",
+    "EIDO.maxSharpePeriod",
+    "PGJ.maxSharpePeriod",
+    "EWY.maxSharpePeriod",
+    "RBL.maxSharpePeriod",
+    "THD.maxSharpePeriod",
+    "RSX.maxSharpePeriod",
+    "GXC.maxSharpePeriod",
+    "EWT.maxSharpePeriod",
+    "EWH.maxSharpePeriod",
+    "EWA.maxSharpePeriod", # iShares MSCI Australia Index Fund	
+    "EPI.maxSharpePeriod", # WisdomTree India Earnings		
+    "GXG.maxSharpePeriod", # Global X/InterBolsa FTSE Colombia 20 ETF		
+    "SPY.maxSharpePeriod", # SPDR S&P 500		
+    "QQQ.maxSharpePeriod", # PowerShares QQQ	
+    "EWM.maxSharpePeriod", # iShares MSCI Malaysia Index Fund		
+    "FXI.maxSharpePeriod", # IShares China Large Cap ETF		
+    "EWC.maxSharpePeriod", # iShares MSCI Canada Index Fund		
+    "EWK.maxSharpePeriod", # iShares MSCI Belgium Index Fund		
+    "IWM.maxSharpePeriod", # iShares Russell 2000 Index Fund		
+    "INP.maxSharpePeriod", # iPath MSCI India ETN		
+    "PIN.maxSharpePeriod", # PowerShares India		
+    "EWJ.maxSharpePeriod", # iShares MSCI Japan Index Fund		
+    "EWO.maxSharpePeriod", # iShares MSCI Austria Index Fund	
+    "EWN.maxSharpePeriod", # iShares MSCI Netherlands Index Fund		
+    "EWS.maxSharpePeriod", # iShares MSCI Singapore Index Fund		
+    "TUR.maxSharpePeriod", # iShares MSCI Turkey Invest Mkt Index		
+    "EWG.maxSharpePeriod", # iShares MSCI Germany Index Fund		
+    "EWW.maxSharpePeriod", # iShares MSCI Mexico Index Fund	
+    "EWQ.maxSharpePeriod", # iShares MSCI France Index Fund	
+    "EWD.maxSharpePeriod", # iShares MSCI Sweden Index Fund	
+    "EZA.maxSharpePeriod", # iShares MSCI South Africa Index Fund	
+    "EWL.maxSharpePeriod", # iShares MSCI Switzerland Index Fund	
+    "EWU.maxSharpePeriod", # iShares MSCI United Kingdom Index Fund	
+    "EIS.maxSharpePeriod", # iShares MSCI Israel Cap Invest Mkt Index 
+    "EWP.maxSharpePeriod", # iShares MSCI Spain Index Fund  
+    "EWI.maxSharpePeriod" # iShares MSCI Italy Index Fund	
+  )
+colnames(allsymbols.maxSharpePeriod) <- c("Long lookback period (weeks)", "Short lookback period (weeks)")
+write.csv(allsymbols.maxSharpePeriod, file = "allsymbols-optimalperiods.csv")
